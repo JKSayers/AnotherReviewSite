44b889e36ece5901ac17527627e4106499cb1d35
JKSayers <jsayers1980@gmail.com>
2017-02-12 16:37:30 -0500

    Added models: Review and Category

---
+++ AnotherReviewSite/Models/Review.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AnotherReviewSite.Models
+{
+    public class Review
+    {
+
+        [Key] //Primary Key for Review table
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishDate { get; set; }
+
+
+        [ForeignKey("Category")]  // Foreign Key connecting to Category table
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
